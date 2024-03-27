30980ae82af19a6fb86b8726fa4447f5a1f34bd5
Florian Froese <f.froese@gmail.com>
2024-03-27 11:11:50 -0700

    Update lab_setup.sh

--- lab_setup.sh
+++ lab_setup.sh
@@ -3,10 +3,11 @@
 # Setup lab
 #
 SEAT=$1
+TA=l430-ta2
+open https://github.com/login?login=$TA
 open https://my.cloudmanager.adobe.com/
 open https://bit.ly/l430-seats
 open https://github.com/S24L430/quality-$SEAT
 open https://github.com/S24L430/frontend-$SEAT
 open https://github.com/S24L430/configuration-$SEAT
 open https://github.com/S24L430/site-$SEAT
-
